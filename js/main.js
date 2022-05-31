fa666de09d91669279b53dd31a9832a5f60994ba
Tim Chiang <tychiang922@gmail.com>
2022-05-31 19:53:57 +0000

    Added function to add li entry to DOM

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* global data */
 var $title = document.querySelector('#title');
 var $photo = document.querySelector('#photo');
 var $note = document.querySelector('#note-input');
@@ -24,3 +25,32 @@ function imgUpdate(event) {
 
 $form.addEventListener('submit', saveAction);
 $photo.addEventListener('keyup', imgUpdate);
+
+function appendEntryToDOM() {
+  var $li = document.createElement('li');
+  var $rowDiv = document.createElement('div');
+  $rowDiv.setAttribute('class', 'row');
+
+  var $imgColDiv = document.createElement('div');
+  $imgColDiv.setAttribute('class', 'column-half mb-20p mt-20p');
+
+  var $imgElement = document.createElement('img');
+  $imgElement.setAttribute('src', 'images/placeholder-image-square.jpg');
+
+  var $txtColDiv = document.createElement('div');
+  $txtColDiv.setAttribute('class', 'column-half pt-20p mb-20p entry-txt');
+
+  var $h3Title = document.createElement('h3');
+  $h3Title.textContent = 'hello';
+  $txtColDiv.append($h3Title);
+  var $pNotes = document.createElement('p');
+  $pNotes.textContent = 'testing';
+  $txtColDiv.append($pNotes);
+
+  $imgColDiv.append($imgElement);
+  $rowDiv.append($imgColDiv);
+  $rowDiv.append($txtColDiv);
+  $li.append($rowDiv);
+}
+
+appendEntryToDOM();
