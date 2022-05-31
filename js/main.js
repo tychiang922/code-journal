5a99f0ceb96cd156806ed54556286e2353221282
Tim Chiang <tychiang922@gmail.com>
2022-05-31 00:22:38 +0000

    changed userInputObj to array of obj and adapted listeners to change

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 var $input = document.querySelectorAll('input');
 var $saveButton = document.querySelector('.save-button');
+var $title = document.querySelector('.title');
+var $photo = document.querySelector('.photo');
+var $img = document.querySelector('img');
 
-function rejectEmptyInput(event) {
+var userInputObj = [{}];
+
+function saveAction(event) {
   for (var inputNode = 0; inputNode < $input.length; inputNode++) {
     if ($input[inputNode] === '') {
       event.preventDefault();
     }
   }
+  event.preventDefault();
+  var imgSrc = $photo.value;
+  userInputObj[userInputObj.length - 1].imgFile = imgSrc;
+  console.log(userInputObj);
 }
 
-$saveButton.addEventListener('click', rejectEmptyInput);
+$saveButton.addEventListener('click', saveAction);
