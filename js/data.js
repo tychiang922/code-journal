70895292a76f5ffa70f052888a7eb55acfa54485
Tim Chiang <tychiang922@gmail.com>
2022-05-31 17:35:42 +0000

    moved local storage to data file and cleaned up saveAction function

--- js/data.js
+++ js/data.js
@@ -6,3 +6,15 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousDataJSON = localStorage.getItem('userData');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+function storage(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('userData', dataJSON);
+}
+
+window.addEventListener('beforeunload', storage);
--- js/main.js
+++ js/main.js
@@ -3,29 +3,24 @@ var $title = document.querySelector('#title');
 var $photo = document.querySelector('#photo');
 var $note = document.querySelector('#note-input');
 var $img = document.querySelector('img');
+var $form = document.querySelector('form');
 
 function saveAction(event) {
   event.preventDefault();
-  data.entries.push({});
-  data.entries[data.nextEntryId - 1].imgSrc = $photo.value;
-  data.entries[data.nextEntryId - 1].title = $title.value;
-  data.entries[data.nextEntryId - 1].notes = $note.value;
+  var dataObject =
+  {
+    imgSrc: $photo.value,
+    title: $title.value,
+    notes: $note.value
+  };
+  data.entries.push(dataObject);
   data.nextEntryId++;
-
-  $photo.value = '';
-  $title.value = '';
-  $note.value = '';
+  $form.reset();
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
-  storage(data);
 }
 
 function imgUpdate(event) {
   $img.setAttribute('src', $photo.value);
-}
-
-function storage(input) {
-  var dataJSON = JSON.stringify(input);
-  localStorage.setItem('userData', dataJSON);
 }
 
 $saveButton.addEventListener('click', saveAction);
