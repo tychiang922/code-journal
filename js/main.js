89d50507082b2d5b7dfb66713bbda89b82b9dd7c
Tim Chiang <tychiang922@gmail.com>
2022-06-01 04:16:06 +0000

    properly edits entries when submit button is pressed

--- js/main.js
+++ js/main.js
@@ -6,20 +6,45 @@ var $img = document.querySelector('img');
 var $form = document.querySelector('form');
 
 $form.addEventListener('submit', function saveAction(event) {
-  event.preventDefault();
-  var dataObject = {
-    imgSrc: $photo.value,
-    title: $title.value,
-    notes: $note.value,
-    id: data.nextEntryId
-  };
-  data.entries.unshift(dataObject);
-  data.nextEntryId++;
-  $form.reset();
-  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
-  var newEntry = appendEntryToDOM(data.entries[0]);
-  $ul.prepend(newEntry);
-  switchToEntries(event);
+  if (data.view === 'entry-form') {
+    event.preventDefault();
+    var dataObject = {
+      imgSrc: $photo.value,
+      title: $title.value,
+      notes: $note.value,
+      id: data.nextEntryId
+    };
+    data.entries.unshift(dataObject);
+    data.nextEntryId++;
+    $form.reset();
+    $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+    var newEntry = appendEntryToDOM(data.entries[0]);
+    $ul.prepend(newEntry);
+    switchToEntries(event);
+  }
+  if (data.view === 'edit-entry') {
+    event.preventDefault();
+    var $liAll = document.querySelectorAll('li');
+    for (var dataIndex = 0; dataIndex < data.entries.length; dataIndex++) {
+      if (data.entries[dataIndex].id === data.editing.id) {
+        data.editing = {
+          imgSrc: $photo.value,
+          title: $title.value,
+          notes: $note.value,
+          id: data.entries[dataIndex].id
+        };
+        data.entries.splice(dataIndex, 1, data.editing);
+        var editedEntry = appendEntryToDOM(data.entries[dataIndex]);
+        $liAll[dataIndex].replaceWith(editedEntry);
+        switchToEntries(event);
+        break;
+      }
+    }
+  }
+  var $i = document.querySelectorAll('i');
+  for (var i = 0; i < $i.length; i++) {
+    $i[i].addEventListener('click', switchToEditEntry);
+  }
 });
 
 $photo.addEventListener('keyup', function imgUpdate(event) {
@@ -82,12 +107,16 @@ $newButton.addEventListener('click', switchToEntryForm);
 
 if (data.view === 'entry') {
   switchToEntries();
-} else {
+} if (data.view === 'entry-form') {
   switchToEntryForm();
+} if (data.view === 'edit-entry') {
+  switchToEditEntry();
 }
 
 function switchToEditEntry(event) {
-  switchToEntryForm();
+  $entries.setAttribute('class', 'view hidden');
+  $entryForm.setAttribute('class', 'view');
+  data.view = 'edit-entry';
   var $h1 = document.querySelector('h1');
   $h1.textContent = 'Edit Entry';
   var dataEntryId = parseInt(event.target.getAttribute('data-entry-id'));
@@ -100,8 +129,9 @@ function switchToEditEntry(event) {
   $photo.value = data.editing.imgSrc;
   $title.value = data.editing.title;
   $note.value = data.editing.notes;
-
 }
 
-var $i = document.querySelector('i');
-$i.addEventListener('click', switchToEditEntry);
+var $i = document.querySelectorAll('i');
+for (var i = 0; i < $i.length; i++) {
+  $i[i].addEventListener('click', switchToEditEntry);
+}
